02c33fe5f3e888d6d8dbed6dfb6dfb09c2f4dd82
Zubia <rashzubia@gmail.com>
2022-09-03 17:29:31 +0200

    Footer finished

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,36 @@
+import { BsFacebook } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
+import { AiOutlineCopyrightCircle } from "react-icons/ai"
+
 const Footer = () => {
-    return <div>Footer</div>;
+    // footer
+    return (
+        // footer
+        <div className="w-full flex text-center lg:text-center bg-palette-80 text-palette-50">
+            <div className="w-3/12">
+                <img
+                    src="./images/deutschifyLogo.png"
+                    className="w-12 m-2"
+                    alt=""
+                />
+                <div className="flex m-3 space-x-2">
+                    <div classname="">
+                        <BsFacebook />
+                    </div>
+                    <div classname="m-2">
+                        <BsInstagram />
+                    </div>
+                </div>
+            </div>
+            <div className="w-6/12">
+                <button className="text-palette-50 m-2 p-2">ABOUT US</button>
+                <button className="text-palette-50 m-2 p-2">CONTACT US</button>
+                <button className="text-palette-50 m-2 p-2">IMPRESSUM</button>
+                <div className="w-12/12 m-2 flex  justify-center gap-1">deutschify <AiOutlineCopyrightCircle /> 2022</div>
+            </div>
+            {/* Copyright */}
+        </div>
+    );
 };
 
 export default Footer;
