295e8501b6b04d0103ddb76e3f13d07006d5776e
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-09-05 11:10:39 +0200

    tailewind fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,20 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                palette: {
-                    10: '#D88C9A',
-                    20: '#DF9D9E',
-                    30: '#8F7B8C',
-                    40: '#394775',
-                    50: '#F8F0E7',
-                    60: '#C9D9D0',
-                    70: '#99C1B9',
-                    80: '#5C7A75',
-                    90: '#1E3231'
-                }
-            }
+export const content = ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"];
+export const theme = {
+    extend: {
+        colors: {
+            palette: {
+                10: "#D88C9A",
+                20: "#DF9D9E",
+                30: "#8F7B8C",
+                40: "#394775",
+                50: "#F8F0E7",
+                60: "#C9D9D0",
+                70: "#99C1B9",
+                80: "#5C7A75",
+                90: "#1E3231",
+            },
         },
     },
-    plugins: [],
 };
+export const plugins = [];
