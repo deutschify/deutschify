7ce724c0d751f0053e76c19d5f4ae3ae4bd51392
Zubia Rashid <92801217+zubia77@users.noreply.github.com>
2022-09-05 07:38:59 +0200

    Zubia: footer
    * startet creating Footer, Colors of Palette added in tailwindconfig.js, name of logo-image changed to camelCase, because of space inbetween
    
    * Footer finished
    
    * correction in tailwind code
    
    * another correction

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                palette: {
+                    10: '#D88C9A',
+                    20: '#DF9D9E',
+                    30: '#8F7B8C',
+                    40: '#394775',
+                    50: '#F8F0E7',
+                    60: '#C9D9D0',
+                    70: '#99C1B9',
+                    80: '#5C7A75',
+                    90: '#1E3231'
+                }
+            }
+        },
     },
     plugins: [],
 };
