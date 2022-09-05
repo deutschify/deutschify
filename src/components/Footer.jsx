5ad3d90a0b60bde0595d264700c2dd67f80a30a1
Zubia <rashzubia@gmail.com>
2022-09-05 13:37:24 +0200

    Facebook and instaaccount connected

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import { BsFacebook } from "react-icons/bs";
 import { BsInstagram } from "react-icons/bs";
-import { AiOutlineCopyrightCircle } from "react-icons/ai"
+import { AiOutlineCopyrightCircle } from "react-icons/ai";
 
 const Footer = () => {
     // footer
@@ -14,19 +14,29 @@ const Footer = () => {
                     alt=""
                 />
                 <div className="flex m-3 space-x-2">
-                    <div className="">
+                    <a
+                        href="https://www.facebook.com/profile.php?id=100085315394768"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <BsFacebook />
-                    </div>
-                    <div className="">
+                    </a>
+                    <a
+                        href="https://www.instagram.com/deutschify.integrationscoach/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <BsInstagram />
-                    </div>
+                    </a>
                 </div>
             </div>
             <div className="w-6/12">
                 <button className="text-palette-50 m-2 p-2">ABOUT US</button>
                 <button className="text-palette-50 m-2 p-2">CONTACT US</button>
                 <button className="text-palette-50 m-2 p-2">IMPRESSUM</button>
-                <div className="w-12/12 m-2 flex  justify-center gap-1">deutschify <AiOutlineCopyrightCircle /> 2022</div>
+                <div className="w-12/12 m-2 flex  justify-center gap-1">
+                    deutschify <AiOutlineCopyrightCircle /> 2022
+                </div>
             </div>
             {/* Copyright */}
         </div>
