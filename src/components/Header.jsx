1b62a803d751f8ca997f5713e3ed50ba15c7c812
Zubia <rashzubia@gmail.com>
2022-09-05 15:02:10 +0200

    general Settings

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,45 @@
-
+import { BsSearch } from "react-icons/bs";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return <div className="w-full bg-palette-80 text-palette-50 mt-4">
-         <img
-                    src="./images/deutschifyLogo.png"
-                    className="w-40 p-2"
-                    alt=""
-                />
-
-                <div className="w-">
-                    
+    return (
+        <div className="w-full bg-palette-80 text-palette-50 mt-4 flex">
+            <img
+                src="./images/deutschifyLogo.png"
+                className="w-40 p-2"
+                alt=""
+            />
+            <div className="flex items-center ">
+                <div className="flex h-10  bg-palette-50 text-palette-80 rounded-3xl p-2 m-4 items-center">
+                    <input
+                        className="bg-transparent"
+                        type="text"
+                        placeholder="Suche..."
+                    />
+                    <BsSearch className="flex text-xl" />
                 </div>
-    </div>;
+            </div>
+            <nav className="flex items-center m-5">
+                <NavLink to="#" className="m-4">
+                    zu den Aufgaben
+                </NavLink>
+                <NavLink to="#" className="m-4">
+                    Wörterbuch
+                </NavLink>
+                <NavLink to="#" className="m-4">
+                    Forum
+                </NavLink>
+            </nav>
+            <div className="flex items-center m-5   ">
+                <div className="bg-palette-10 p-4 text-2xl py-2 rounded-full">
+                    A
+                </div>
+            </div>
+            <div className="flex items-center m-10">
+                <div className="bg-palette-10 p-4 text-xl py-2 rounded-full">Login</div>
+            </div>
+        </div>
+    );
 };
 
 export default Header;
