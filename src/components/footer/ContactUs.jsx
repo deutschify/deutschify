1fb5b168d8e52cbb874fecb3c1912c599f3eda7f
Zubia Rashid <92801217+zubia77@users.noreply.github.com>
2022-09-09 17:03:53 +0200

    Zubi contact us
    * created contact us site with email js
    
    * Popup window for a thank you
    
    * message sent popup
    
    Co-authored-by: Mezekir Abraham Ghebreyesus <71440482+Mez-A@users.noreply.github.com>

--- src/components/footer/ContactUs.jsx
+++ src/components/footer/ContactUs.jsx
@@ -1,44 +1,108 @@
-import emailjs from 'emailjs-com'
 
+import emailjs from "emailjs-com";
+import { useState } from "react";
+import ContactPopup from "./ContactPopop";
 
 const ContactUs = () => {
-    
+    const [isOpen, setIsOpen] = useState(false);
+
 
     function sendEmail(e) {
         e.preventDefault();
 
-    emailjs.sendForm('service_sibh6aj', 'template_s5ji57q', e.target, 'r4VXiStFKFZmc17at')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset()
+
+        emailjs
+            .sendForm(
+                "service_sibh6aj",
+                "template_s5ji57q",
+                e.target,
+                "r4VXiStFKFZmc17at"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        e.target.reset();
     }
+
+    const togglePopup = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <div className="bg-palette-50 h-max p-4">
-            <h1 className="text-center text-3xl">Wir freuen uns von dir zu hören!</h1>
+            <h1 className="text-center text-3xl">
+                Wir freuen uns von dir zu hören!
+            </h1>
             <h2 className="text-center text-2xl">Euer direkter Draht zu uns</h2>
             <div className="flex justify-center p-10">
-                <form onSubmit={sendEmail} className="bg-palette-60  md:w-2/4 md:h-full md:p-10 flex justify-center rounded-2xl">
+                <form
+                    onSubmit={sendEmail}
+                    className="bg-palette-60  md:w-2/4 md:h-full md:p-10 flex justify-center rounded-2xl"
+                >
                     <div className=" ">
                         <div className="m-6">
-                            <input type="text" className="w-96 h-10 p-2 rounded-full" placeholder="Name" name="from_name"/>
+                            <input
+                                type="text"
+                                className="w-96 h-10 p-2 rounded-full"
+                                placeholder="Name"
+                                name="from_name"
+                            />
                         </div>
                         <div className="m-6">
-                            <input type="email" className="w-96 h-10 p-2 rounded-full" placeholder="Email Adresse" name="reply_to"/>
+                            <input
+                                type="email"
+                                className="w-96 h-10 p-2 rounded-full"
+                                placeholder="Email Adresse"
+                                name="reply_to"
+                            />
                         </div>
                         <div className="m-6">
-                            <textarea className="w-96 h-60 p-2 rounded-2xl" name="message" id="" cols="30" rows="10" placeholder="Tippe deine Nachricht hier..."></textarea>
+                            <textarea
+                                className="w-96 h-60 p-2 rounded-2xl"
+                                name="message"
+                                id=""
+                                cols="30"
+                                rows="10"
+                                placeholder="Tippe deine Nachricht hier..."
+                            ></textarea>
                         </div>
-                        <div className="m-6">
-                            <input type="submit" className="w-96 h-10 p-2 rounded-full bg-palette-70" value="send message"/>
+                        <div className="m-6 relative">
+                            <input
+                                type="submit"
+                                className="w-96 h-10 p-2 rounded-full bg-palette-70"
+                                value="send message"
+                                onClick={togglePopup}
+                            />
+                            <div className="absolute bottom-40"> 
+                                {isOpen && (
+                                    <ContactPopup
+                                        content={
+                                            <>
+                                                <b>
+                                                    Danke für deine Nachricht!
+                                                </b>
+                                                <p>
+                                                    Wir werden dein Anliegen
+                                                    zeitnah bearbeiten.
+                                                </p>
+                                            </>
+                                        }
+                                        handleClose={togglePopup}
+                                    />
+                                )}
+                            </div>
+
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-    )
+    );
 };
 
 export default ContactUs;
