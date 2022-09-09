085254b72a405642c83693afa3d3b88857a90041
Zubia <rashzubia@gmail.com>
2022-09-09 16:53:23 +0200

    message sent popup

--- src/components/footer/ContactPopop.jsx
+++ src/components/footer/ContactPopop.jsx
@@ -1,12 +1,13 @@
-const ContactPopup = props => {
+const ContactPopup = (props) => {
     return (
-      <div className="w-40 h-36 bg-palette-10 text-palette-90">
-        <div className="">
-          <span className="" onClick={props.handleClose}>x</span>
-          {props.content}
+        <div className="z-10 flex justify-center">
+            <div className="w-11/12 h-36 bg-palette-10/75 text-palette-50 text-center p-8 rounded-xl border-8 border-palette-70" onClick={props.handleClose}>
+          
+                    {props.content}
+               
+            </div>
         </div>
-      </div>
     );
-  };
-   
-  export default ContactPopup;
+};
+
+export default ContactPopup;
--- src/components/footer/ContactUs.jsx
+++ src/components/footer/ContactUs.jsx
@@ -1,7 +1,6 @@
-import emailjs from 'emailjs-com'
-import {useState} from 'react'
-import ContactPopup from './ContactPopop'
-
+import emailjs from "emailjs-com";
+import { useState } from "react";
+import ContactPopup from "./ContactPopop";
 
 const ContactUs = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,52 +8,97 @@ const ContactUs = () => {
     function sendEmail(e) {
         e.preventDefault();
 
-    emailjs.sendForm('service_sibh6aj', 'template_s5ji57q', e.target, 'r4VXiStFKFZmc17at')
-      .then((result) => {
-          console.log(result.text);
-      }, (error) => {
-          console.log(error.text);
-      });
-      e.target.reset()
+        emailjs
+            .sendForm(
+                "service_sibh6aj",
+                "template_s5ji57q",
+                e.target,
+                "r4VXiStFKFZmc17at"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+        e.target.reset();
     }
 
     const togglePopup = () => {
         setIsOpen(!isOpen);
-      }
+    };
 
     return (
         <div className="bg-palette-50 h-max p-4">
-            <h1 className="text-center text-3xl">Wir freuen uns von dir zu hören!</h1>
+            <h1 className="text-center text-3xl">
+                Wir freuen uns von dir zu hören!
+            </h1>
             <h2 className="text-center text-2xl">Euer direkter Draht zu uns</h2>
             <div className="flex justify-center p-10">
-                <form onSubmit={sendEmail} className="bg-palette-60  md:w-2/4 md:h-full md:p-10 flex justify-center rounded-2xl">
+                <form
+                    onSubmit={sendEmail}
+                    className="bg-palette-60  md:w-2/4 md:h-full md:p-10 flex justify-center rounded-2xl"
+                >
                     <div className=" ">
                         <div className="m-6">
-                            <input type="text" className="w-96 h-10 p-2 rounded-full" placeholder="Name" name="from_name"/>
+                            <input
+                                type="text"
+                                className="w-96 h-10 p-2 rounded-full"
+                                placeholder="Name"
+                                name="from_name"
+                            />
                         </div>
                         <div className="m-6">
-                            <input type="email" className="w-96 h-10 p-2 rounded-full" placeholder="Email Adresse" name="reply_to"/>
+                            <input
+                                type="email"
+                                className="w-96 h-10 p-2 rounded-full"
+                                placeholder="Email Adresse"
+                                name="reply_to"
+                            />
                         </div>
                         <div className="m-6">
-                            <textarea className="w-96 h-60 p-2 rounded-2xl" name="message" id="" cols="30" rows="10" placeholder="Tippe deine Nachricht hier..."></textarea>
+                            <textarea
+                                className="w-96 h-60 p-2 rounded-2xl"
+                                name="message"
+                                id=""
+                                cols="30"
+                                rows="10"
+                                placeholder="Tippe deine Nachricht hier..."
+                            ></textarea>
                         </div>
-                        <div className="m-6">
-                            <input type="submit" className="w-96 h-10 p-2 rounded-full bg-palette-70" value="send message" onClick={togglePopup}/>
-                            
-    {isOpen && <ContactPopup
-      content={<>
-        <b>Design your Popup</b>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <button>Test button</button>
-      </>}
-      handleClose={togglePopup}
-    />}
+                        <div className="m-6 relative">
+                            <input
+                                type="submit"
+                                className="w-96 h-10 p-2 rounded-full bg-palette-70"
+                                value="send message"
+                                onClick={togglePopup}
+                            />
+                            <div className="absolute bottom-40"> 
+                                {isOpen && (
+                                    <ContactPopup
+                                        content={
+                                            <>
+                                                <b>
+                                                    Danke für deine Nachricht!
+                                                </b>
+                                                <p>
+                                                    Wir werden dein Anliegen
+                                                    zeitnah bearbeiten.
+                                                </p>
+                                            </>
+                                        }
+                                        handleClose={togglePopup}
+                                    />
+                                )}
+                            </div>
                         </div>
                     </div>
                 </form>
             </div>
         </div>
-    )
+    );
 };
 
 export default ContactUs;
