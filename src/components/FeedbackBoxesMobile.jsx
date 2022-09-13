68bbaf12796f334ba902b5357fc4111bb064ea85
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-09-13 16:40:48 +0200

    feedback boxes are there

---
+++ src/components/FeedbackBoxesMobile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const FeedbackBoxesMobile = () => {
+    return (
+        <div className=" grid grid-rows-3 justify-center justify-items-center  md:hidden">
+            <div
+                className="w-48 h-48
+  border-box border-black border-2"
+            >
+                4
+            </div>
+            <div
+                className="w-48 h-48
+ border-box border-black border-2"
+            >
+                5
+            </div>
+            <div
+                className="w-48 h-48
+ border-box border-black border-2"
+            >
+                6
+            </div>
+        </div>
+    );
+};
+
+export default FeedbackBoxesMobile;
