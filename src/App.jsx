c5a18b9f3966138f9676351306a506c874604a7d
Zubia <rashzubia@gmail.com>
2022-09-13 15:58:02 +0200

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ import A2 from "./components/A2";
 import B1 from "./components/B1";
 function App() {
     return (
-        <div className="bg-palette-60 pb-24 h-max">
+        <div className="bg-palette-60 pb-24 h-max w-screen">
             <Header />
             <Routes>
                 <Route path="/" element={<Homepage />} />
