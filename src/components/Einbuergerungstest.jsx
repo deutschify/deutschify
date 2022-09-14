0c933c75d113db274dbc556c4c4752e432373cae
Mez-A <dimoabr@gmail.com>
2022-09-14 12:30:31 +0200

    fetch questions from backend

--- src/components/Einbuergerungstest.jsx
+++ src/components/Einbuergerungstest.jsx
@@ -1,4 +1,17 @@
+import {useParams} from 'react-router-dom'
+import axios from 'axios';
+import {baseUrl} from '../App'
+
 const Einbuergerungstest = () => {
+
+    const {bundesland} = useParams();
+
+    const fetchQuestions = async () => {
+        const questions = (await axios.get(`${baseUrl}/${bundesland}`)).data
+        console.log(baseUrl);
+        console.log(questions);
+    }
+
     return (
         <div className=" bg-palette-80 m-10 shadow-bs border-4 border-palette-80 rounded-xl ">
             <h1 className="text-center text-2xl m-10 text-palette-50">
@@ -9,7 +22,7 @@ const Einbuergerungstest = () => {
             </h2>
             <div className="bg-palette-50  rounded-xl p-4 flex flex-col items-center justify-center mx-16 my-8 shadow-bs border-4 border-palette-60 md:flex-row md:flex-wrap md:w-11/12 md:justify-center " >
                 <div className="bg-palette-60 w-80 h-20 rounded-xl border-palette-50 border-2 m-2 flex justify-center items-center text-xl text-palette-50 hover:bg-palette-50 hover:border-palette-60 hover:text-palette-60 font-block3 font-bold">
-                    Baden-Württemberg
+                    <button onClick={()=> fetchQuestions()}>Baden-Württemberg</button> 
                 </div>
                 <div className="bg-palette-60 w-80 h-20 rounded-xl border-palette-50 border-2 m-2 flex justify-center items-center text-xl text-palette-50 hover:bg-palette-50 hover:border-palette-60 hover:text-palette-60 font-block3 font-bold">
                     Bayern
