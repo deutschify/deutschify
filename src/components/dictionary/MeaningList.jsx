32ce72ff6ab61b8aedbdcf572ecb6fc53e598dc7
sali-Taymour <103145903+sali-taymour@users.noreply.github.com>
2022-09-16 09:00:18 +0200

    styles in dictionary
    * styles
    
    * added pictures
    
    * dictionary-style
    
    Co-authored-by: sali <sali.taymor@gmail.com>

--- src/components/dictionary/MeaningList.jsx
+++ src/components/dictionary/MeaningList.jsx
@@ -4,7 +4,7 @@ const MeanigList = ({ mean }) => {
     <div>
       {mean.map(val => val.meanings.map(means => means.definitions.map(def => (
         <div key={def.definition}>
-          <li>{def.definition}</li>
+          <li className='bg-palette-20'>{def.definition}</li>
           <hr />
         </div>
       ))))}
