6f7356ae931f5542057b2be93f1417b6e7a79cc1
Zubia <rashzubia@gmail.com>
2022-09-17 10:55:05 +0200

    corr

--- src/components/orientierung/LernbereichOrientierung.jsx
+++ src/components/orientierung/LernbereichOrientierung.jsx
@@ -1,36 +1,37 @@
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
 import axios from "axios";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
 const Lernbereich = () => {
+    const [questions, setQuestions] = useState("");
+    // const [imageUrl, setImageUrl] = useState("")
+    const { bundesland } = useParams();
+    console.log(bundesland);
 
-        const [questions, setQuestions] = useState("")
-        // const [imageUrl, setImageUrl] = useState("")
-        const { bundesland } = useParams()
-        
- 
-        useEffect(() => {
-            const fetchData = async () => {
-              const response = await axios.get(`${baseUrl}/questions${bundesland}`).data
-              const questions = await response.json()
-              setQuestions(questions)
-            }
-            fetchData()
-          })
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await axios.get(
+                `${baseUrl}/all-questions/${bundesland}`
+            );
+            const questions = await response.send();
+            setQuestions(questions);
+        };
+        fetchData();
+    });
 
-       
-    return ( 
-        <div className=""><h1>Lernbereich { cat }</h1>
-       <div className="">
-           {/* <img src={imageUrl} alt="" /> */}
-              {/*    {imageUrl && setImageUrl(jsonFromDB.image)} */}
+    return (
+        <div className="">
+            <h1>Lernbereich {bundesland}</h1>
+            <div className="">
+                {/* <img src={imageUrl} alt="" /> */}
+                {/*    {imageUrl && setImageUrl(jsonFromDB.image)} */}
 
-         
-       </div>
+                
+            </div>
         </div>
-     );
-    };
- 
+    );
+};
+
 export default Lernbereich;
