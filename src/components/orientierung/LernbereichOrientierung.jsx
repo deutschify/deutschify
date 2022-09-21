5ee37c77fb67bd585f04e3df0ab758fe77dc1866
Zubia <rashzubia@gmail.com>
2022-09-21 10:46:19 +0200

    back button created

--- src/components/orientierung/LernbereichOrientierung.jsx
+++ src/components/orientierung/LernbereichOrientierung.jsx
@@ -1,6 +1,7 @@
 import { useParams, NavLink } from "react-router-dom";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Pagination } from 'rsuite';
 import LiDExc from "../orientierung/LiDExc";
 import LiDMod from "../orientierung/LiDMod";
 
@@ -8,7 +9,7 @@ const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
 const Lernbereich = () => {
     const [questions, setQuestions] = useState([]);
-    // const [imageUrl, setImageUrl] = useState("")
+    const [activePage, setActivePage] = useState(1);
     const { category } = useParams();
 
     useEffect(() => {
@@ -19,30 +20,8 @@ const Lernbereich = () => {
             const questions = await response.json();
             setQuestions(questions);
         };
-
-        //   const fetchDataDeutschland = async () => {
-        //     const response = await fetch(
-        //         `${baseUrl}/all-questions/deutschland`
-        //     );
-        //     const questions = await response.json();
-        //     setQuestions(questions);
-        // };
         fetchDataBundesland();
-        // fetchDataDeutschland();
     }, []);
-
-    //       const fetchData = async () => {
-    //     const response = await Promise.all([
-
-    //         fetch(`${baseUrl}/all-questions/${category}`).then((response) => response.json()),
-    //         fetch(`${baseUrl}/all-questions/deutschland`).then((response) => response.json())
-
-    //     ]);
-    //     let questions = await response.json();
-    //     setQuestions(questions);
-    // };
-    // fetchData();
-    //   });
 
     return (
         <div className="border-4 border-palette-50 m-2 p-4 rounded-xl text-center">
@@ -57,9 +36,19 @@ const Lernbereich = () => {
                 >
                     Zum Übungstest
                 </NavLink>
-                <div className="text-palette-80 p-10">
+                <div className="p-4">
+
+                    {/* Searchbar für Testfragen */}
+                    <div className="">
+
+                    </div>
+                       <div className="text-palette-80">
                     {questions.length} Fragen
                 </div>
+                
+                </div>
+             
+
                 <NavLink
                     to={`/lernbereich/${category}/modelltest`}
                     element={<LiDMod />}
@@ -69,8 +58,6 @@ const Lernbereich = () => {
                 </NavLink>
             </nav>
             <div className="bg-palette-80 rounded-xl border-4 border-palette-50">
-                {/* <img src={imageUrl} alt="" /> */}
-                {/*    {imageUrl && setImageUrl(jsonFromDB.image)} */}
                 <div className="flex flex-col items-center text-palette-60 p-6">
                     <div className="text-2xl capitalize">
                         Fragen zu {category}
@@ -85,6 +72,7 @@ const Lernbereich = () => {
                                 <div className="text-palette-60  ">
                                     {question.number}
                                 </div>
+                                
                                 <div className="text-palette-60 m-2 p-2">
                                     {" "}
                                     <div className="mb-2 border-b-2 border-palette-60 ">
@@ -102,6 +90,21 @@ const Lernbereich = () => {
                         );
                     })}
                 </div>
+                     <Pagination
+        prev
+        last
+        next
+        first
+        size="lg"
+        // total={310}
+        // limit={30}
+        pages={10}
+        activePage={activePage}
+        onChangePage={setActivePage}
+       
+      />
+ 
+         
             </div>
         </div>
     );
