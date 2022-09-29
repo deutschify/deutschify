ab5d019121541ed8f04f3b3cf6002b66160f7174
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-09-29 11:59:44 +0200

    fetch all posts from backend

--- src/components/forum/news-feed/NewsFeed.jsx
+++ src/components/forum/news-feed/NewsFeed.jsx
@@ -3,20 +3,38 @@ import DropDownMenu from "../DropDownMenu";
 import NavForum from "../NavForum";
 import Post from "../post/Post";
 import Share from "../share/Share";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const NewsFeed = () => {
+    const backend_base_url = "http://localhost:8000";
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const response = await axios.get(
+                backend_base_url + "/posts/news-feed/all"
+            );
+            setPosts(response.data);
+        };
+        fetchPosts();
+    }, []);
+
     return (
         <div>
             <DropDownMenu />
             <NavForum />
             <div className="feedWrapper flex flex-col justify-center items-center pt-8 pb-6 ">
                 <Share />
+                {posts.map((p) => (
+                    <Post key={p.id} post={p} />
+                ))}
+                {/* <Post />
                 <Post />
                 <Post />
                 <Post />
                 <Post />
-                <Post />
-                <Post />
+                <Post /> */}
             </div>
 
             {/* <main className="newsFeed">
