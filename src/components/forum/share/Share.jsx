6c85fc8db08db8183847e4f41563406f7f7690d4
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-10-04 14:57:06 +0200

    post from frontend

--- src/components/forum/share/Share.jsx
+++ src/components/forum/share/Share.jsx
@@ -1,5 +1,5 @@
 import { MdPermMedia } from "react-icons/md";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 
 import { useStore } from "../../../store";
@@ -8,12 +8,32 @@ const share = () => {
     const backend_base_url = "http://localhost:8000";
     // const [user, setUser] = useState({});
 
+    //fetching the current user
     const fetchCurrentUser = useStore((state) => state.fetchCurrentUser);
     const currentUser = useStore((state) => state.currentUser);
 
+    //useRef for th share input box
+    const desc = useRef();
+    //useState for the onChange event
+    const [file, setFiles] = useState(null);
+
+    //to submit a post
+
+    const submitPostHandler = async (e) => {
+        e.preventDefault();
+        const newPost = {
+            userId: currentUser._id,
+            desc: desc.current.value,
+        };
+        try {
+            await axios.post(backend_base_url + `/posts`, newPost);
+        } catch (error) {}
+    };
+
     useEffect(() => {
         fetchCurrentUser();
-        console.log(currentUser);
+        // console.log(currentUser);
+        // console.log(currentUser._id);
     }, []);
 
     useEffect(() => {
@@ -36,29 +56,39 @@ const share = () => {
                     <input
                         className="shareInput outline-none w-4/5 rounded-xl ml-8 pl-2 h-12 break-words "
                         placeholder={`Stell us eine Frage, ${currentUser.firstName} ...`}
+                        ref={desc}
                     />
                 </div>
                 <hr className="shareHr m-5 border-2 border-palette-40 " />
-                <div className="shareBottom flex items-center justify-between">
+                <form
+                    className="shareBottom flex items-center justify-between"
+                    onSubmit={submitPostHandler}
+                >
                     <div className="shareOptions flex ml-5 ">
-                        <div className="shareOption flex items-center cursor-pointer ">
+                        <label
+                            htmlFor="file"
+                            className="shareOption flex items-center cursor-pointer "
+                        >
                             <MdPermMedia className="shareIcon  mr-0.5 text-palette-80" />
-                            <span className="shareOptionText m-3 text-sm">
-                                Fotos oder Videos
-                            </span>
                             <input
-                                className="inputImage hidden"
+                                className="inputImage hidden "
                                 id="file"
                                 type="file"
                                 accept=".png, .jpg, .jpeg"
-                                onChange={(e) => setFile(e.target.files[0])}
+                                onChange={(e) => setFiles(e.target.files[0])}
                             />
-                        </div>
+                            <span className="shareOptionText m-3 text-sm">
+                                Fotos oder Videos
+                            </span>
+                        </label>
                     </div>
-                    <button className="shareButton outline-none p-1.5 rounded-md bg-palette-40 mr-5 ">
+                    <button
+                        className="shareButton outline-none p-1.5 rounded-md bg-palette-40 mr-5"
+                        type="submit"
+                    >
                         Posten
                     </button>
-                </div>
+                </form>
             </div>
         </div>
     );
