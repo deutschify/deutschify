a37483152d26be511fcd40d4df9aa3d93d9f26ae
sali <sali.taymor@gmail.com>
2022-10-10 17:19:26 +0200

    letters

--- src/components/sprachkurs/SchreibenB1.jsx
+++ src/components/sprachkurs/SchreibenB1.jsx
@@ -1,17 +1,12 @@
-import React, { useState, useRef } from "react";
-
+import React, { useEffect,useState, useRef } from "react";
+import _letters from "./letters.json";
+console.log(_letters);
 const SchreibenB1 = () => {
     const dragItem = useRef();
     const dragOverItem = useRef();
-    const [list, setList] = useState([
-        "Item 1",
-        "Item 2",
-        "Item 3",
-        "Item 4",
-        "Item 5",
-        "Item 6",
-        "Item 7",
-    ]);
+    const [showlist, setshowlist] = useState(false);
+    const [letters, setletters] = useState(_letters);
+    const [showdiv, setshowdiv] = useState("");
 
     const dragStart = (e, position) => {
         dragItem.current = position;
@@ -32,21 +27,45 @@ const SchreibenB1 = () => {
         dragOverItem.current = null;
         setList(copyListItems);
     };
+    const ShowList = (e, title) => {
+        e.currentTarget;
+
+        setshowlist(true), setshowdiv(title);
+       
+    };
+    useEffect(() => {
+        
+         console.log(letters);
+    })
+
     return (
         <>
-            {list &&
-                list.map((item, index) => (
-                    <div className="flex justify-center  align-center bg-palette-80 p-2   "
-                        
-                        onDragStart={(e) => dragStart(e, index)}
-                        onDragEnter={(e) => dragEnter(e, index)}
-                        onDragEnd={drop}
-                        key={index}
-                        draggable
-                    >
-                        {item}
-                    </div>
-                ))}
+            {letters.map((letter, index) => {
+                return (
+                    <>
+                        {" "}
+                        <button onClick={(e) => ShowList(e, letter.title)}>
+                            {" "}
+                            {letter.title}
+                        </button>
+                        {showlist && showdiv === letter.title  &&
+                            letter.items.map((item, index) => {
+                                return (
+                                    <div
+                                        className="flex justify-center  align-center bg-palette-80 p-2   "
+                                        onDragStart={(e) => dragStart(e, index)}
+                                        onDragEnter={(e) => dragEnter(e, index)}
+                                        onDragEnd={drop}
+                                        key={index}
+                                        draggable
+                                    >
+                                        {item}
+                                    </div>
+                                );
+                            })}
+                    </>
+                );
+            })}
         </>
     );
 };
