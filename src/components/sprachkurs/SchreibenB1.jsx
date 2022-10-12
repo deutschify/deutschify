52b17dbee3e9767b0b1aad2eb357e0944fc8da4a
sali <sali.taymor@gmail.com>
2022-10-12 09:42:43 +0200

    drag and drop

--- src/components/sprachkurs/SchreibenB1.jsx
+++ src/components/sprachkurs/SchreibenB1.jsx
@@ -1,46 +1,48 @@
 
-import React, { useEffect,useState, useRef } from "react";
-
-import _letters from "./letters.json";
-console.log(_letters);
-const SchreibenB1 = () => {
+import React, { useState, useRef } from "react";
+const Schreiben = () => {
     const dragItem = useRef();
     const dragOverItem = useRef();
-    const [showlist, setshowlist] = useState(false);
-    const [letters, setletters] = useState(_letters);
-    const [showdiv, setshowdiv] = useState("");
+    const [list, setList] = useState([
+        "----------------------Beschwerde-----------------------",
+        "Sehr geehrte Damen und Herren,",
+        "vor einem Monat habe ich bei Ihnen eine Waschmaschine Marke WM 1200 RS bestellt. Jetzt ist sie kaputt, deshalb schreibe ich Ihnen.",
+        " Ich habe dieses Modell gekauft, weil es im Angebot war. Ich war am Anfang sehr zufrieden, aber jetzt bin ich enttäuscht.",
+        "Das Gerät startet nicht, obwohl die Lampe leuchtet. Ich würde gern das Gerät umtauschen. Könnten Sie bitte die kaputte Waschmaschine abholen und eine neue bringen?",
+        "Ich wäre auch mit einer Reparatur einverstanden, aber der Techniker soll zu uns kommen.",
+        "Über eine schnelle Antwort würde ich mich sehr freuen.",
+        "Mit freundlichen Grüßen",
+        "-----------------------------Eiladung--------------------------------------",
 
+        "Sehr geehrter {name}, ",
+        "Es ist eine Weile her! Ich hoffe dir gehts gut.",
+        "ch würde Sie gerne zu einer Geburtstagsfeier für meine Tochter {name} nächsten Samstag, {date}, einladen.",
+    
+        " Wir werden kein Nein als Antwort akzeptieren, versprochen! Wenn Sie eine Mitfahrgelegenheit benötigen,",
+        "holt Jeanine Sie gerne ab. Teilen Sie ihr einfach Ihre Adresse mit und sie wird Sie dort treffen.",
+        "Ich hoffe sehr, dass Sie es schaffen. Es wäre toll, dich wiederzusehen.",
+        "Beste, {Name des Absenders}",
+    ]);
     const dragStart = (e, position) => {
         dragItem.current = position;
         console.log(e.target.innerHTML);
     };
-
     const dragEnter = (e, position) => {
         dragOverItem.current = position;
         console.log(e.target.innerHTML);
     };
-
     const drop = (e) => {
-        const copyListItems = [...letters];
+        const copyListItems = [...list];
         const dragItemContent = copyListItems[dragItem.current];
         copyListItems.splice(dragItem.current, 1);
         copyListItems.splice(dragOverItem.current, 0, dragItemContent);
         dragItem.current = null;
         dragOverItem.current = null;
-        setletters(copyListItems);
+        setList(copyListItems);
     };
-    const ShowList = (e, title) => {
-        e.currentTarget;
-
-        setshowlist(true), setshowdiv(title);
-    };
-
-    useEffect(() => {
-        console.log(_letters);
-    });
 
     return (
-        <>
+        <>  
             <div className="float-right mt-40 ">
                 <img
                     src="/public/images/construction.png"
@@ -48,48 +50,29 @@ const SchreibenB1 = () => {
                     className="w-[35rem] bg-palette-40 m-4 border-4 border-palette-60 rounded-xl  "
                 />
             </div>
-            <div className="gird grid-cols-3 justify-between ">
-                {letters.map((letter, index) => {
-                    return (
-                        <>
-                            <div className="">
-                                {" "}
-                                <button
-                                    class="w-40 text-center border-4 border-palette-60 bg-palette-70 p-4 text-xl rounded-xl shadow-outer hover:shadow-inner hover:text-palette-50 m-5 active"
-                                    onClick={(e) => ShowList(e, letter.title)}
-                                >
-                                    {" "}
-                                    {letter.title}
-                                </button>
-                                {showlist &&
-                                    showdiv === letter.title &&
-                                    letter.items.map((item, index) => {
-                                        return (
-                                            <div
-                                                className="flex  justify-center align-center w-[50%]  bg-palette-80 p-2  rounded-xl py-2 border"
-                                                onDragStart={(e) =>
-                                                    dragStart(e, index)
-                                                }
-                                                onDragEnter={(e) =>
-                                                    dragEnter(e, index)
-                                                }
-                                                onDragEnd={drop}
-                                                key={index}
-                                                draggable
-                                            >
-                                                {item}
-                                            </div>
-                                        );
-                                    })}
+            <div className='sm:flex flex-col justify-center align-center'>
+            {list &&
+                list.map((item, index) => (
+                    <>
+                      
+                            <div
+                                className="w-[35rem] bg-palette-40 m-4 border-4 border-palette-60 rounded-xl  "
+                                onDragStart={(e) => dragStart(e, index)}
+                                onDragEnter={(e) => dragEnter(e, index)}
+                                onDragEnd={drop}
+                                key={index}
+                                draggable
+                            >
+                                {item}
                             </div>
-                        </>
-                    );
-                })}
-            </div>
-
-        </>
+                        
+                        
+                        
+                    </>
+                ))}
+                <hr />
+       </div> </>
     );
 };
-
-export default SchreibenB1;
-
+export default Schreiben;
+//     
