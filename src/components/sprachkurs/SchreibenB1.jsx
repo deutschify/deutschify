2cb9819b4e1d9991a002ac5f88929d28502179ee
sali <sali.taymor@gmail.com>
2022-10-12 13:41:15 +0200

    letters  styles

--- src/components/sprachkurs/SchreibenB1.jsx
+++ src/components/sprachkurs/SchreibenB1.jsx
@@ -4,7 +4,9 @@ const Schreiben = () => {
     const dragItem = useRef();
     const dragOverItem = useRef();
     const [list, setList] = useState([
-        "----------------------Beschwerde-----------------------",
+        
+        "______________________________Beschwerde_______________________________",
+
         "Sehr geehrte Damen und Herren,",
         "vor einem Monat habe ich bei Ihnen eine Waschmaschine Marke WM 1200 RS bestellt. Jetzt ist sie kaputt, deshalb schreibe ich Ihnen.",
         " Ich habe dieses Modell gekauft, weil es im Angebot war. Ich war am Anfang sehr zufrieden, aber jetzt bin ich enttäuscht.",
@@ -12,12 +14,12 @@ const Schreiben = () => {
         "Ich wäre auch mit einer Reparatur einverstanden, aber der Techniker soll zu uns kommen.",
         "Über eine schnelle Antwort würde ich mich sehr freuen.",
         "Mit freundlichen Grüßen",
-        "-----------------------------Eiladung--------------------------------------",
+        "______________________________Einladung_______________________________",
 
         "Sehr geehrter {name}, ",
         "Es ist eine Weile her! Ich hoffe dir gehts gut.",
         "ch würde Sie gerne zu einer Geburtstagsfeier für meine Tochter {name} nächsten Samstag, {date}, einladen.",
-    
+
         " Wir werden kein Nein als Antwort akzeptieren, versprochen! Wenn Sie eine Mitfahrgelegenheit benötigen,",
         "holt Jeanine Sie gerne ab. Teilen Sie ihr einfach Ihre Adresse mit und sie wird Sie dort treffen.",
         "Ich hoffe sehr, dass Sie es schaffen. Es wäre toll, dich wiederzusehen.",
@@ -42,21 +44,20 @@ const Schreiben = () => {
     };
 
     return (
-        <>  
+        <>
             <div className="float-right mt-40 ">
                 <img
                     src="/public/images/construction.png"
                     alt
-                    className="w-[35rem] bg-palette-40 m-4 border-4 border-palette-60 rounded-xl  "
+                    className="w-[35rem] coaster border-palette-60 rounded-xl  "
                 />
             </div>
-            <div className='sm:flex flex-col justify-center align-center'>
-            {list &&
-                list.map((item, index) => (
-                    <>
-                      
+            <div className="sm:flex flex-col cover ml-6  justify-center align-center w-[50%]">
+                {list &&
+                    list.map((item, index) => (
+                        <>
                             <div
-                                className="w-[35rem] bg-palette-40 m-4 border-4 border-palette-60 rounded-xl  "
+                                className="w-[35rem] m-4  bg-palette-60 rounded-3xl p-3 text-palette-50 placeholder:text-palette-50/75 border-4 border-palette-80 shadow-inner focus:outline-none"
                                 onDragStart={(e) => dragStart(e, index)}
                                 onDragEnter={(e) => dragEnter(e, index)}
                                 onDragEnd={drop}
@@ -65,13 +66,11 @@ const Schreiben = () => {
                             >
                                 {item}
                             </div>
-                        
-                        
-                        
-                    </>
-                ))}
+                        </>
+                    ))}
                 <hr />
-       </div> </>
+            </div>{" "}
+        </>
     );
 };
 export default Schreiben;
