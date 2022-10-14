aa5e085e51c2d6f090642befcaca1e0c32847a30
sali <sali.taymor@gmail.com>
2022-10-14 22:55:43 +0200

    schriftliche ausdruck-style works

--- src/components/sprachkurs/SchreibenB1.jsx
+++ src/components/sprachkurs/SchreibenB1.jsx
@@ -5,25 +5,24 @@ const Schreiben = () => {
     const dragOverItem = useRef();
     const [list, setList] = useState([
         
-        "______________________________Beschwerde_______________________________",
+        "______________Beschwerde______________",
 
-        "Sehr geehrte Damen und Herren,",
         "vor einem Monat habe ich bei Ihnen eine Waschmaschine Marke WM 1200 RS bestellt. Jetzt ist sie kaputt, deshalb schreibe ich Ihnen.",
-        " Ich habe dieses Modell gekauft, weil es im Angebot war. Ich war am Anfang sehr zufrieden, aber jetzt bin ich enttäuscht.",
+        "Sehr geehrte Damen und Herren,",
         "Das Gerät startet nicht, obwohl die Lampe leuchtet. Ich würde gern das Gerät umtauschen. Könnten Sie bitte die kaputte Waschmaschine abholen und eine neue bringen?",
         "Ich wäre auch mit einer Reparatur einverstanden, aber der Techniker soll zu uns kommen.",
-        "Über eine schnelle Antwort würde ich mich sehr freuen.",
         "Mit freundlichen Grüßen",
-        "______________________________Einladung_______________________________",
-
-        "Sehr geehrter {name}, ",
+        "Über eine schnelle Antwort würde ich mich sehr freuen.",
+        " Ich habe dieses Modell gekauft, weil es im Angebot war. Ich war am Anfang sehr zufrieden, aber jetzt bin ich enttäuscht.",
+        "___________Einladung___________",
         "Es ist eine Weile her! Ich hoffe dir gehts gut.",
         "ch würde Sie gerne zu einer Geburtstagsfeier für meine Tochter {name} nächsten Samstag, {date}, einladen.",
-
         " Wir werden kein Nein als Antwort akzeptieren, versprochen! Wenn Sie eine Mitfahrgelegenheit benötigen,",
-        "holt Jeanine Sie gerne ab. Teilen Sie ihr einfach Ihre Adresse mit und sie wird Sie dort treffen.",
-        "Ich hoffe sehr, dass Sie es schaffen. Es wäre toll, dich wiederzusehen.",
+        "Sehr geehrter {name}, ",
+
         "Beste, {Name des Absenders}",
+        "Ich hoffe sehr, dass Sie es schaffen. Es wäre toll, dich wiederzusehen.",
+        "holt Jeanine Sie gerne ab. Teilen Sie ihr einfach Ihre Adresse mit und sie wird Sie dort treffen.",
     ]);
     const dragStart = (e, position) => {
         dragItem.current = position;
@@ -45,19 +44,19 @@ const Schreiben = () => {
 
     return (
         <>
-            <div className="float-right mt-40 ">
+            <div className="hidden md:block float-right m-20 mt-60">
                 <img
                     src="/public/images/construction.png"
                     alt
                     className="w-[35rem] coaster border-palette-60 rounded-xl  "
                 />
             </div>
-            <div className="sm:flex flex-col cover ml-6  justify-center align-center w-[50%]">
+            <div className="flex flex-col cover  justify-center align-center md:w-[50%] m-10 text-center">
                 {list &&
                     list.map((item, index) => (
                         <>
                             <div
-                                className="w-[35rem] m-4  bg-palette-60 rounded-3xl p-3 text-palette-50 placeholder:text-palette-50/75 border-4 border-palette-80 shadow-inner focus:outline-none"
+                                className="m-4  bg-palette-60 rounded-3xl p-3 text-palette-50 placeholder:text-palette-50/75 border-4 border-palette-80  focus:outline-none "
                                 onDragStart={(e) => dragStart(e, index)}
                                 onDragEnter={(e) => dragEnter(e, index)}
                                 onDragEnd={drop}
