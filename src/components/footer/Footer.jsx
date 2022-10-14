b1f953b68c70d7dc8db69d971a2b845456f8d59a
Zubia Rashid <92801217+zubia77@users.noreply.github.com>
2022-10-15 01:52:56 +0200

    footer setting finished (#69)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,17 +12,17 @@ const Footer = () => {
     // footer
     return (
         // footer
-        <div className="w-full flex items-center text-center bg-palette-50 text-palette-60 fixed overflow-hidden  bottom-0 z-20 shadow-outer">
-            <div className="w-3/12">
+        <div className="w-full  flex items-center text-center bg-palette-50 text-palette-60 fixed overflow-hidden  bottom-0 z-20 shadow-outer">
+            <div className="w-4/12 md:w-1/12">
                 <NavLink className="" to="/" element={<Homepage />}>
                     <img
                         src={LogoSide}
-                        className="w-40 m-2"
+                        className="m-2"
                         alt=""
                     />
                 </NavLink>
             </div>
-            <div className="w-11/12 m-2 md:w-6/12 md:text-lg">
+            <div className="w-10/12 text-sm m-2 md:w-11/12 md:text-lg">
                 <NavLink
                     className="text-palette-60 m-2 p-2 hover:text-palette-80"
                     to="/about-us"
@@ -48,11 +48,11 @@ const Footer = () => {
                     bewerte uns{" "}
                 </NavLink>
 
-                <div className=" w-full m-2 flex justify-center gap-1">
+                <div className="text-xxs flex justify-center gap-1">
                     deutschify <AiOutlineCopyrightCircle /> 2022
                 </div>
             </div>{" "}
-            <div className="flex items-center mx-4 w-3/12 space-x-4 md:justify-end md:text-2xl">
+            <div className="flex items-center justify-end mx-4 w-1/12 space-x-4  md:text-2xl">
                 <a
                     className="hover:text-palette-80"
                     href="https://www.facebook.com/profile.php?id=100085315394768"
