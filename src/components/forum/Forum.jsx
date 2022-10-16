93ecebf2b1954172f365e3bd155f18dac77c67ed
Omar-Tabaa <omar.tabaa90@gmail.com>
2022-10-16 19:44:05 +0200

    commented the searchBar

--- src/components/forum/Forum.jsx
+++ src/components/forum/Forum.jsx
@@ -24,7 +24,6 @@ const Forum = () => {
                             element={<PostEdit />}
                         />
                     </Routes>
-                    {/* <AboutForum /> */}
                 </div>
             ) : (
                 <>
